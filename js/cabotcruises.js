1cce8fe0ec8e5da67b1baacc6464533c0f701764
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-02-10 01:54:39 -0700

    add ld+json data

--- js/cabotcruises.js
+++ js/cabotcruises.js
@@ -3,6 +3,27 @@
 
 /*global alert*/
 
+const apiURL = "api.openweathermap.org/data/2.5/weather?zip=84058,us&appid=2f4dcd39f2b81b6e284a6acac29fcac9"
+
+
+
+fetch ( apiURL) 
+.then((mydata) => mydata.json())
+.then((mydata) => {
+  console.log(mydata); 
+}); 
+
+
+
+
+
+fetch ('./data/cabotcruises.json') 
+.then((mydata) => mydata.json())
+.then((mydata) => {
+  console.log(mydata); 
+}); 
+
+
 
 function toggleMenu() {
   document.getElementById("primaryNav").classList; 
