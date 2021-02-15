cc19bb51174d72508ef17e953b79e6a3fc78eada
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-02-15 15:30:08 -0700

    remove bootstrap

--- js/cabotcruises.js
+++ js/cabotcruises.js
@@ -3,16 +3,6 @@
 
 /*global alert*/
 
-const apiURL = "api.openweathermap.org/data/2.5/weather?zip=84058,us&appid=2f4dcd39f2b81b6e284a6acac29fcac9"
-
-
-
-fetch ( apiURL) 
-.then((mydata) => mydata.json())
-.then((mydata) => {
-  console.log(mydata); 
-}); 
-
 
 
 
@@ -48,6 +38,3 @@ $(document).ready(function(){
    arrows: false
   });
 });
-
-
-$('.single-item').slick();
